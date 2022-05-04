c3346aa0507cb3e1f09f8cf5b89abbaeec785c01
KIMKICHAN <rlcks980705@gmail.com>
2022-05-04 17:48:32 +0900

    refactor : props 변경

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
-function TodoList({ todos, setTodos }) {
+function TodoList({ todos, deleteTodo, changeTodo }) {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
         <TodoListItem
           todo={todo}
           key={todo.id}
-          setTodos={setTodos}
-          todos={todos}
+          deleteTodo={deleteTodo}
+          changeTodo={changeTodo}
         />
       ))}
     </div>
   );
 }
 
-export default TodoList;
+export default React.memo(TodoList);
